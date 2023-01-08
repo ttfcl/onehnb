8471a1d24694070fb48530898acbc7f1319d6b7d
ttfcl <ktx1016@naver.com>
2023-01-08 16:03:35 +0900

    1차 반영

---
+++ docs/nav.js
@@ -0,0 +1,13 @@
+let nav1 = document.querySelector(".nav1")
+let nav2 = document.querySelector(".nav2")
+
+
+window.addEventListener('scroll', function(){
+    if(window.scrollY > 500) {
+        nav1.classList.add("hide")
+        nav2.classList.add("hide")
+    }else if(window.scrollY <= 500){
+        nav1.classList.remove("hide")
+        nav2.classList.remove("hide")
+    }
+});
