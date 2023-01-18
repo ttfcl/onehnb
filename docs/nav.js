9e52553adb1c6aa59bffe71e68ebd00a8c5793ab
ttfcl <ktx1016@naver.com>
2023-01-18 12:35:58 +0900

    recall

--- docs/nav.js
+++ docs/nav.js
@@ -3,10 +3,10 @@ let nav2 = document.querySelector(".nav2")
 
 
 window.addEventListener('scroll', function(){
-    if(window.scrollY > 500) {
+    if(window.scrollY > 300) {
         nav1.classList.add("hide")
         nav2.classList.add("hide")
-    }else if(window.scrollY <= 500){
+    }else if(window.scrollY <= 300){
         nav1.classList.remove("hide")
         nav2.classList.remove("hide")
     }
